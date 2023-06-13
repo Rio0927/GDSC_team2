f0cb052dabef8ef9893804b819c3537105b4b94b
h16k <95564547+h16k@users.noreply.github.com>
2023-06-13 17:41:12 +0900

    データベースの設計修正

--- credit_calculator/models.py
+++ credit_calculator/models.py
@@ -1,11 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# 教授のモデル
-class Professor(models.Model):
-    last_name = models.CharField(max_length=50)  # 教授の姓
-    first_name = models.CharField(max_length=50)  # 教授の名
-
 # 科目群のモデル
 class Genre(models.Model):
     name = models.CharField(max_length=20)  # 科目群の名前
@@ -29,11 +24,26 @@ class Course(models.Model):
     minimum_grade_level = models.IntegerField(default=1)  # 受講可能な最低学年
 
 # 科目のインスタンスのモデル
-class CourseInstance(models.Model):
-    # FIRST_TERM = 0
-    # SECOND_TERM = 1
-    # SUMMER = 2
-    # ALL_YEAR = 3
+# class CourseInstance(models.Model):
+#     # FIRST_TERM = 0
+#     # SECOND_TERM = 1
+#     # SUMMER = 2
+#     # ALL_YEAR = 3
+#     FIRST_TERM = "前期"
+#     SECOND_TERM = "後期"
+#     SUMMER = "夏季集中"
+#     ALL_YEAR = "通年"
+#     TERM_CHOICES = [
+#         (FIRST_TERM, '前期'),
+#         (SECOND_TERM, '後期'),
+#         (SUMMER, '夏季集中'),
+#         (ALL_YEAR, '通年'),
+#     ]
+#     course = models.ForeignKey(Course, on_delete=models.CASCADE)  # 科目への参照
+#     semester = models.CharField(max_length=20, choices=TERM_CHOICES, default=FIRST_TERM)
+
+# 科目のスケジュールのモデル
+class CourseSchedule(models.Model):
     FIRST_TERM = "前期"
     SECOND_TERM = "後期"
     SUMMER = "夏季集中"
@@ -44,17 +54,7 @@ class CourseInstance(models.Model):
         (SUMMER, '夏季集中'),
         (ALL_YEAR, '通年'),
     ]
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)  # 科目への参照
-    semester = models.CharField(max_length=20, choices=TERM_CHOICES, default=FIRST_TERM)
 
-
-# 科目と教授の関連性のモデル
-class CourseProfessor(models.Model):
-    course_instance = models.ForeignKey(CourseInstance, on_delete=models.CASCADE, related_name='professors')  # 科目のインスタンスへの参照
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)  # 教授への参照
-
-# 科目のスケジュールのモデル
-class CourseSchedule(models.Model):
     NULL = "なし"
     MONDAY = "月"
     TUESDAY = "火"
@@ -73,10 +73,23 @@ class CourseSchedule(models.Model):
         (SATURDAY, 'Saturday'),
     ]
 
-    course_instance = models.ForeignKey(CourseInstance, on_delete=models.CASCADE, related_name='schedules')  # 科目のインスタンスへの参照
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='schedules')  # 科目への参照
+    semester = models.CharField(max_length=20, choices=TERM_CHOICES, default=FIRST_TERM)
     day_of_week = models.CharField(max_length=5, choices=DAY_OF_WEEK_CHOICES)  # 曜日
     period = models.IntegerField()  # 時間帯
-    classroom = models.CharField(max_length=50, null=True, blank=True) #教室
+    classroom = models.CharField(max_length=50, blank=True) #教室
+
+# 教授のモデル
+class Professor(models.Model):
+    last_name = models.CharField(max_length=50)  # 教授の姓
+    first_name = models.CharField(max_length=50)  # 教授の名
+
+# 科目と教授の関連性のモデル
+class CourseProfessor(models.Model):
+    course_schedule = models.ForeignKey(CourseSchedule, on_delete=models.CASCADE, related_name='professors')  # 科目スケジュールへの参照
+    # course_instance = models.ForeignKey(CourseInstance, on_delete=models.CASCADE, related_name='professors')  # 科目のインスタンスへの参照
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)  # 教授への参照
+
 
 # ユーザーのプロフィールのモデル
 class UserProfile(models.Model):
@@ -87,11 +100,11 @@ class UserProfile(models.Model):
 # ユーザーのタイムテーブルのモデル
 class Timetable(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)  # ユーザープロフィールへの参照
-    course_instance = models.ForeignKey(CourseInstance, on_delete=models.CASCADE)  # 科目のインスタンスへの参照
+    course_instance = models.ForeignKey(CourseSchedule, on_delete=models.CASCADE)  # 科目のインスタンスへの参照
     grade = models.IntegerField()
     semester = models.IntegerField()
 
 # ユーザーの「いいね」のモデル
 class Like(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)  # ユーザープロフィールへの参照
-    course_instance = models.ForeignKey(CourseInstance, on_delete=models.CASCADE)  # 科目のインスタンスへの参照
+    course_instance = models.ForeignKey(CourseSchedule, on_delete=models.CASCADE)  # 科目のインスタンスへの参照
